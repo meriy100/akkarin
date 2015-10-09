2f0a2e61fc9525015a85b1c7a5ba1e6105057eb7
meriy100 <ttattataa@gmail.com>
2015-10-09 22:28:18 +0900

    migrate commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009132212) do
+ActiveRecord::Schema.define(version: 20151009132711) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "color_id"
+    t.integer  "user_id"
+    t.integer  "wallet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "colors", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +35,14 @@ ActiveRecord::Schema.define(version: 20151009132212) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
