db0205ea2d171adc44275c11a575a453c7ee011c
meriy100 <ttattataa@gmail.com>
2015-10-24 18:31:54 +0900

    finish modal form

--- app/models/short_cc.rb
+++ app/models/short_cc.rb
@@ -7,11 +7,16 @@ class ShortCc < ActiveRecord::Base
   accepts_nested_attributes_for :short_cps, :allow_destroy => true
 
   after_save :set_user, if: ->{!self.user.present?}
+  after_save :set_category, if: ->{!self.category.present?}
 
   def set_user
     self.user = self.sub_category.try(:user) || self.sub_category.try(:category).try(:user)
     self.save
   end
 
+  def set_category
+    self.category = self.sub_category.try(:category)
+    self.save
+  end
 
 end
