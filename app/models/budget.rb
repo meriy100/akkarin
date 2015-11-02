9f063dadb106317caca8059d857f55fe6ef1841b
meriy100 <ttattataa@gmail.com>
2015-11-03 01:55:25 +0900

    budget presence

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,5 +1,5 @@
 class Budget < ActiveRecord::Base
-  validates :user_id, :category_id, :price, :started_date, :finished_date, precense: true
+  validates :user_id, :category_id, :price, :started_date, :finished_date, precence: true
   belongs_to :user
   belongs_to :category
   belongs_to :sub_category
