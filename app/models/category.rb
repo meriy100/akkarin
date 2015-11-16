532af6e13cbd116a3da43dba0a4e465f5a5b8d30
meriy100 <ttattataa@gmail.com>
2015-11-16 11:31:24 +0900

    #21 service controller 枠組みだけ

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,7 +12,7 @@ class Category < ActiveRecord::Base
   has_many :budgets
   accepts_nested_attributes_for :sub_categories, :allow_destroy => true
   accepts_nested_attributes_for :short_ccs, :allow_destroy => true
-  
+
   validates :from_wallet_id, presence: true, if: Proc.new{|r| r.record_type != Record::INCOME}
   validates :to_wallet_id, presence: true, if: Proc.new{|r| r.record_type != Record::PAYMENT}
 end
