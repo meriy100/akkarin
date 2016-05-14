4abb14d3ae9e6450ac68998651064d18a5892d32
meriy100 <ttattataa@gmail.com>
2016-05-14 22:33:12 +0900

    ??

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-///= require jquery.turbolinks
-///= require jquery_ujs
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require twitter/bootstrap
+//= require angular.min
+//= require app
 //= require_tree .
 //= require turbolinks
 
